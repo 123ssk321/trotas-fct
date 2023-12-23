6939d8ed027eeba36d4db6e389b16d57e8ef0bfd
SahilKumar <91741510+123ssk321@users.noreply.github.com>
2023-12-23 00:55:04 +0000

    Added second version project files

--- src/OperatingSystem.java
+++ src/OperatingSystem.java
@@ -1,22 +1,29 @@
-/*
- * @author Sahil Satish Kumar
+/**
+ * @author Sahil Satish Kumar e Tiago Vieira
  * Esta classe controla as classes do cliente e da trotinete e as informacoes relativas
  * ao sistema de aluguer
- * permite a interacao entre a classe Main e a classe Client e Trot
+ * permite a interacao entre a classe Main e as os objetos Cliente e Trotinete armazenados nas
+ * classes ClientSet e TrotSet
  */
 public class OperatingSystem {
 	//Constantes
 	private static final int PRICE = 100, LEGALTIME = 60,  EXTRATIME = 30;
 	
 	//Variaveis de instancia
+	private Rect campusFCT;
 	private Client cl, tmpCl;
 	private Trot tr,tmpTr;
-	private int totalDelayTime, numberOfRents, totalMoney;
+	private ClientSet clients;
+	private TrotSet trots;
+	private int totalDelayTime, numberOfRents, totalMoney, trotsWithLoc;
 	private int tmpTotalDelayTime, tmpNumberOfRents, tmpTotalMoney;
 	private boolean isDiscounted;
 	
 	//Construtor
 	public OperatingSystem() {
+		clients = new ClientSet();
+		trots = new TrotSet();
+		campusFCT = new Rect();
 		cl= new Client("","","",""); 
 		tr = new Trot("","");
 		tmpCl = new Client(cl);
@@ -28,52 +35,59 @@ public class OperatingSystem {
 	}
 	
 	//Metodos que permitem a interacao entre a classe Main e a classe Client e Trot
-	public Client callClient() {
-		return cl;
+	public Client callClient(String nif) {
+		return clients.getClient(nif);
 	}
-	public Trot callTrot() {
-		return tr;
+	/**
+	 * Devolve a trotinete com o ID dado
+	 * @param id, ID da trotinete a devolver
+	 * @return
+	 */
+	public Trot callTrot(String id) {
+		return trots.getTrot(id);
 	}
-	
 	//Metodos modificadores
 	
 	/*Pre: nif!="" && email!="" && phoneNum!="" && name!="" */
 	public void addClient(String nif, String email, String phoneNum, String name) {
-		cl = new Client(nif, email, phoneNum, name);
+		clients.addToSet(nif, email, phoneNum, name);
 	}
-	public void remClient() {
-		cl = null;
+	public void remClient(String nif) {
+		clients.remFromSet(nif);;
 	}
 	public void addTrot(String id, String license) {
-		tr = new Trot(id, license);
+		trots.addToSet(id, license);
 	}
-	/*
-	 * Inicia um aluguer de uma trotinete
-	 * Associa o nif e o nome do cliente a trotinete
-	 * Associa o id e a matricula da trotintete ao cliente
+	/**
+	 * Inicia o aluguer de uma trotinete, chamando os metodos de inicio de aluguer do cliente
+	 * e da trotinete
+	 * @param cl, cliente que vai iniciar o aluguer
+	 * @param tr, trotinete que vai ser alugada
 	 */
-	public void rentTrot() {
-		tr.rent(cl.showNif(), cl.showName());
-		cl.rentBegin(tr.showIdTrot(), tr.showLicensePlate());
+	public void rentTrot(Client cl, Trot tr) {
+		tr.rent(cl);
+		cl.rentBegin(tr);
 	}
-	/*
-	 * Acaba o aluguer de uma trotinete, guardando os dados do aluguer anterior(relativos
-	 * ao cliente, a trotinete e ao sistema) antes de chamar o metodo para terminar o aluguer
-	 * Atualizas variaveis de instancia do sistema
+	/**
+	 * Acaba o aluguer de uma trotinete, calculando o dinheiro gasto e chamando os metodos para terminar o aluguer
+	 * Atualiza os dados do sistema
+	 * @param id, ID da trotinete que vai deixar de estar alugada
+	 * @param minutes, tempo de aluguer em minutos
+	 * Pre: minutes > 0
 	 */
-	public void releaseTrot(int minutes) {
+	public void releaseTrot(String id,int minutes) {
 		int delayPrice = 0;
 		int delayTime = 0;
 		
+		Client cl = trots.getTrot(id).showClient(); 
 		tmpCl = new Client(cl);
-		tmpTr = new Trot(tr);
+		tmpTr = new Trot(trots.getTrot(id));
 		tmpTotalDelayTime = totalDelayTime;
 		tmpNumberOfRents = numberOfRents;
 		tmpTotalMoney = totalMoney;
 		isDiscounted = false;
-		
 		numberOfRents++;
-		tr.release(minutes);
+		trots.getTrot(id).release(minutes);;
 		if (minutes<=LEGALTIME) {
 			cl.rentEnd(PRICE, minutes);
 			totalMoney+=PRICE;
@@ -93,15 +107,60 @@ public class OperatingSystem {
 			}
 		}		
 	}
-	/*
-	 * Carrega o saldo do cliente
-	 * Pre: cents>0
+	/**
+	 * Igual ao metodo releaseTrot(String, String) mas no ato de libertar
+	 * acrescenta a informacao da localizacao
+	 * @param id, ID da trotinete que vai deixar de estar alugada
+	 * @param minutes, tempo de aluguer em minutos
+	 * @param x, longitude da trotinete
+	 * @param y, latitude da trotinete
+	 * Pre: minutes > 0
 	 */
-	public void rechargeBalance(int cents) {
-		cl.addBalance(cents);
+	public void releaseTrotWithLoc(String id,int minutes, double x, double y) {
+		int delayPrice = 0;
+		int delayTime = 0;
+		
+		Client cl = trots.getTrot(id).showClient(); 
+		tmpCl = new Client(cl);
+		tmpTr = new Trot(trots.getTrot(id));
+		tmpTotalDelayTime = totalDelayTime;
+		tmpNumberOfRents = numberOfRents;
+		tmpTotalMoney = totalMoney;
+		isDiscounted = false;
+		trotsWithLoc++;
+		numberOfRents++;
+		trots.getTrot(id).releaseWithLoc(minutes, x, y);;
+		if (minutes<=LEGALTIME) {
+			cl.rentEnd(PRICE, minutes);
+			totalMoney+=PRICE;
+		}
+		else {
+			delayTime = minutes-LEGALTIME;
+			totalDelayTime+=delayTime;               //Acumula os tempos de atraso
+			if (delayTime % EXTRATIME == 0 ) {            //Verifica se o tempo de atraso e divisivel pelo EXTRATIME
+				delayPrice = (delayTime / EXTRATIME) * PRICE;      //Calcula o preco a pagar com atraso
+				cl.rentEnd(delayPrice + PRICE, minutes);
+				totalMoney+=(delayPrice + PRICE);
+			}
+			else {
+				delayPrice = (delayTime / EXTRATIME) * PRICE + PRICE;  //Calcula o preco a pagar pelo atraso
+				cl.rentEnd(delayPrice + PRICE, minutes);
+				totalMoney+=(delayPrice + PRICE);
+			}
+		}		
+	}
+	/**
+	 * Chama o metodo do cliente para carregar o saldo
+	 * @param nif, NIF do cliente cujo saldo vai ser carregado
+	 * @param cents, valor em centimos a carregar
+	 * Pre: cents > 0
+	 */
+	public void rechargeBalance(String nif,int cents) {
+		clients.getClient(nif).addBalance(cents);
 	}
 	/*
 	 * Muda os dados do sistema, do cliente e da trotinete para os dados antes do ultimo aluguer efetuado
+	 * Nao usado neste trabalho	 
 	 */
 	public void discount() {
 		cl = tmpCl;
@@ -111,22 +170,68 @@ public class OperatingSystem {
 		numberOfRents = tmpNumberOfRents;
 		totalMoney = tmpTotalMoney;
 	}
-	public void activateTrot() {
-		tr.activate();
+	/**
+	 * @return iterador dos clientes existentes
+	 */
+	public ClientIterator clientIterator() {
+		return clients.iterator();
 	}
-	public void deactivateTrot() {
-		tr.inactive();
+	/**
+	 * @return iterador dos clientes com saldo negativo
+	 */
+	public ClientIterator debtClientIterator() {
+		return clients.debtClientsOrd();
 	}
-	
+	/**
+	 * @return iterador das trotinetes existentes
+	 */
+	public TrotIterator trotIterator() {
+		return trots.iterator();
+	}
 	//Metodos de consulta
-	public int totalDelayTime() {
+	
+	/**
+	 * @return total de minutos de atraso de todos os clientes
+	 */
+	public int totalDelayTime() { 
 		return totalDelayTime;
 	}
+	/**
+	 * @return total de dinheiro gasto no sistema
+	 */
 	public int totalMoneyWon() {
 		return totalMoney;
 	}
+	/**
+	 * @return numero de alugueres efetuados por todos os clientes
+	 */
 	public int totalClientUses() {
 		return numberOfRents;
+	}
+	/**
+	 * @return o numero de trotinetes com localizaco
+	 */
+	public int numberTrotsWithLoc() {
+		return trotsWithLoc;
+	}
+	/**
+	 * Chama o metodo que verifica se a localizacao da trotinete pertence ao campus da FCT
+	 * @param x, longitude da trotinete
+	 * @param y, latitude da trotinete
+	 * @return se a trotinete se encontra dentro do campus
+	 */
+	public boolean isInCampusFCT(double x, double y) {
+		return campusFCT.inCampus(x, y);
+	}
+	/**
+	 * Chama o metodo que cria um vetor com todas as trotinetes que possuem localizacao
+	 * ordenadas por ordem de distancia ao cliente e de seguida devolve um iterador desse vetor
+	 * @param clientX, longitude do cliente
+	 * @param clientY, latitude do cliente
+	 * @return iterador das trotinetes com localizacao
+	 */
+	public TrotIterator trotWithLocIterator(double clientX, double clientY) {
+		return trots.distanceOrdIterartor(clientX, clientY);	
 	}
 	public boolean isDiscounted() {
 		return isDiscounted;
--- src/Trot.java
+++ src/Trot.java
@@ -1,31 +1,49 @@
-/*
- * @author Sahil Satish Kumar
+/**
+ * Um objeto desta classe representa uma trotinete.
+ * @author Sahil Satish Kumar e Tiago Vieira
  * Esta classe manipula as informacoes relativas a trotinete, como por exemplo a atualizacao 
  * das variaveis de instancia
  */
 public class Trot {
 	
 	//Variaveis de instancia
-	private String idTrot, licensePlate, state, clientNif, clientName, functional;
-	private boolean isRented, isActive;
+	private String idTrot, licensePlate, state, functional;
+	private boolean isRented, isActive, withLoc;
 	private int totalRents, totalMovMinutes;
-
+	private double locX, locY, distanceToClient;
+	private Client client;
+	
 	//Construtores
 	 
 	/* Pre: id!="" && license!=""*/
+	/**
+	 * Cria uma nova trotinete com as informacoes dadas
+	 * @param id, ID da nova trotine
+	 * @param license, matricula da nova trotinete
+	 * Pre: id!="" && license!=""
+	 */
 	public Trot(String id, String license) {
 		idTrot = id;
 		licensePlate = license;
 		state = "parada";
 		functional = "activa";
-		clientNif = "";
-		clientName = "";
 		isRented = false;
 		isActive = true;
 		totalRents = 0;
 		totalMovMinutes = 0;
+		distanceToClient = 0;
+		locX=0;
+		locY=0;
+		withLoc = false;
 	}
-	/*Pre: t!=null*/
+	
+	/**
+	 * Igual ao construtor Trot(String, String)
+	 * E usado em metodos onde se torna mais simples criar uma Trotinete temporaria a partir das informacoes da trotinete existente,
+	 * chamada por outro metodo, que queremos utilizar
+	 * @param t, trotinete existente
+	 * Pre: t!= null
+	 */
 	public Trot(Trot t) {
 		idTrot = t.showIdTrot();
 		licensePlate = t.showLicensePlate();
@@ -39,57 +57,92 @@ public class Trot {
 	
 	//Metodos modificadores
 	
-	/*
-	 * Inicia o aluguer da trotinete, atualizando o seu estado 
-	 * Guarda  o nif e o nome do cliente
-	 * Pre: nif!="" && name!=""
+	/**
+	 * Inicia o aluguer da trotinete, atualizando o seu estado
+	 * @param c, cliente que alugou a trotinete
+	 * Pre: c != null;
 	 */
-	public void rent(String nif,String name) {
+	public void rent(Client c) {
 		state = "alugada";
 		isRented = true;
-		clientNif = nif;
-		clientName = name;
+		client = c;
 	}
-	/*
-	 * Acaba o aluguer da trotinete, atualizando o seu estado
-	 * Vai acumulando o numero total de minutos do aluguer e 
-	 * numero total de alugueres da trotinete
+	/**
+	 * Acaba o aluguer da trotinete, atualizando o seu estado e informacoes
+	 * @param minutes, tempo de aluguer em minutos
 	 * Pre: minutes > 0
 	 */
 	public void release(int minutes) {
+		client = null;
 		state = "parada";
 		isRented = false;
 		totalMovMinutes+=minutes;
 		totalRents++;
+		withLoc = false;
 	}
-	/*
+	/**
+	 * Acaba o aluguer da trotinete, atualizando o seu estado, informacoes e localizacao
+	 * @param minutes, tempo de aluguer em minutos
+	 * @param x, longitude da trotinete
+	 * @param y, latitude da trotinete
+	 * Pre: minutes > 0 && x != null && y != null
+	 */
+	public void releaseWithLoc(int minutes, double x, double y) {
+		client = null;
+		state = "parada";
+		isRented = false;
+		totalMovMinutes+=minutes;
+		totalRents++;
+		locX = x;
+		locY = y;
+		withLoc = true;
+	}
+	/**
 	 * Ativa a trotinete, atualizando o seu estado
+	 * Pre: isActive = false
 	 */
 	public void activate() {
 		functional = "activa";
 		isActive = true;
 	}
-	/*
+	/**
 	 * Desativa a trotinete, atualizando o seu estado
+	 * Pre: isRented = false && isActive = true
 	 */
 	public void inactive() {
 		functional = "inactiva";
-		isRented = false;
 		isActive = false;
+	}
+	/**
+	 * Calcula a distancia da trotinete ao cliente
+	 * @param clientX, longitude do cliente
+	 * @param clientY, latitude do cliente
+	 * @return distancia da trotinete ao cliente
+	 * Pre: clientX != null && clientY != null
+	 */
+	public double distanceToClient(double clientX, double clientY) {
+		double distanceX = Math.pow(clientX-locX, 2);
+		double distanceY = Math.pow(clientY-locY, 2);
+		distanceToClient = Math.sqrt(distanceX+distanceY); 
+		return Math.sqrt(distanceX+distanceY); 
+	}
+	/**
+	 * Verifica se o ID da trotinete e maior do que o ID dado
+	 * @param id, ID a comparar com o ID da Trotinete
+	 * @return se o ID da trotiente e maior que o ID dado
+	 * Pre: ID != null
+	 */
+	public boolean greaterThan(String id) {
+		return this.showIdTrot().compareToIgnoreCase(id) > 0; 
 	}
 	
 	//Metodos de consulta
+	
 	public String showIdTrot() {
 		return idTrot;
 	}
 	public String showLicensePlate() {
 		return licensePlate;
-	}
-	public String showClientNif() {
-		return clientNif;
-	}
-	public String showClientName() {
-		return clientName;
 	}
 	public String trotState() {
 		if (this.isTrotActive()) {
@@ -108,11 +161,31 @@ public class Trot {
 	public boolean isTrotActive() {
 		return isActive;
 	}
+	/**
+	 * @return se a trotinete foi libertada com informacao de localizacao
+	 */
+	public boolean releasedWithLoc() {
+		return withLoc;
+	}
 	public int showTotalRents() {
 		return totalRents;
 	}
 	public int showTotalMovingMinutes() {
 		return totalMovMinutes;
 	}
-	
+	public double showLat() {
+		return locX;
+	}
+	public double showLong() {
+		return locY;
+	}
+	/**
+	 * @return cliente que no momento esta a alugar a trotinete
+	 */
+	public Client showClient() {
+		return client;
+	}	
+	public double showDistance() {
+		return distanceToClient;
+	}
 }
